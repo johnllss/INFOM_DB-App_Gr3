de65125a03062336deba9b5218cf84874785a032
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-10-18 19:11:32 +0800

    Small changes

--- user.sql
+++ user.sql
@@ -1,16 +1,29 @@
 USE dbgolf;
 
 CREATE TABLE user (
-	user_id 			BIGINT PRIMARY KEY auto_increment,
+	user_id 			BIGINT PRIMARY KEY AUTO_INCREMENT,
     first_name  		VARCHAR(100) NOT NULL,
     middle_name 		VARCHAR(100) NULL,
     last_name 			VARCHAR(100) NOT NULL,
     email				VARCHAR(255) NOT NULL UNIQUE,
     contact				VARCHAR(20) NULL,
-    membership_tier 	ENUM('Bronze', 'Silver', 'Gold', 'Platinum', 'Diamon'),
+    membership_tier 	ENUM('Bronze', 'Silver', 'Gold', 'Platinum', 'Diamon') DEFAULT 'Bronze',
     membership_start 	DATE,
     membership_end 		DATE,
     months_subscribed 	BIGINT,
     loyalty_points 		BIGINT DEFAULT 0
 );
 
+-- USERS who have just 
+
+-- VALUES with all columns
+INSERT INTO user (first_name, middle_name, last_name, email, contact, membership_tier, membership_start, membership_end, months_subscribed, loyalty_points)
+VALUES
+	();
+    
+-- VALUES without middle_name column
+INSERT INTO user (first_name, last_name, email, contact, membership_tier, membership_start, membership_end, months_subscribed, loyalty_points)
+VALUES
+	();
+    
+-- 
