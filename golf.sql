bb3fc68becd0ba29f8575a2afb831ad80e64633e
gab <101641170+gabespineli@users.noreply.github.com>
2025-10-18 21:13:29 +0800

    Modify payment and staff tables with default values
    Added default value for discount_applied in payment table and status in staff table.

--- golf.sql
+++ golf.sql
@@ -3,23 +3,23 @@ CREATE SCHEMA IF NOT EXISTS dbgolf;
 USE dbgolf;
 
 CREATE TABLE payment (
-	payment_id			  BIGINT PRIMARY KEY AUTO_INCREMENT,
-  total_price			  DECIMAL(8,2) NOT NULL,
-  date_paid			    DATETIME, -- null = not yet paid
-  payment_method		ENUM('Cash on Delivery', 'GCash', 'Credit Card'),
-  status				    ENUM('Cancelled', 'Pending', 'Paid'),
-  discount_applied	DECIMAL(1,2), -- null = unsubscribed
-  FOREIGN KEY 		  (user_id) REFERENCES users(user_id),
-  FOREIGN KEY 		  (session_id) REFERENCES session(session_id),
-  FOREIGN KEY 		  (cart_id) REFERENCES cart(cart_id)
+	payment_id			BIGINT PRIMARY KEY AUTO_INCREMENT,
+  	total_price			DECIMAL(8,2) NOT NULL,
+ 	date_paid			DATETIME, -- null = not yet paid
+  	payment_method		ENUM('Cash on Delivery', 'GCash', 'Credit Card'),
+  	status				ENUM('Cancelled', 'Pending', 'Paid'),
+  	discount_applied	DECIMAL(1,2) DEFAULT 0.0
+  	FOREIGN KEY 		(user_id) REFERENCES users(user_id),
+  	FOREIGN KEY 		(session_id) REFERENCES session(session_id),
+  	FOREIGN KEY 		(cart_id) REFERENCES cart(cart_id)
 );
 
 CREATE TABLE staff (
-  staff_id			  BIGINT PRIMARY KEY AUTO_INCREMENT,
-  name				    VARCHAR(100) NOT NULL,
-  email				    VARCHAR(100) NOT NULL,
-  contact				  VARCHAR(20) NOT NULL,
-  role				    VARCHAR(50) NOT NULL,
-  status				  ENUM('Available', 'Occupied') NOT NULL,
-  service_fee			DECIMAL(8,2) NOT NULL
+  staff_id		BIGINT PRIMARY KEY AUTO_INCREMENT,
+  name			VARCHAR(100) NOT NULL,
+  email			VARCHAR(100) NOT NULL,
+  contact		VARCHAR(20) NOT NULL,
+  role			VARCHAR(50) NOT NULL,
+  status		ENUM('Available', 'Occupied') NOT NULL DEFAULT 'Available',
+  service_fee	DECIMAL(8,2) NOT NULL
 );
