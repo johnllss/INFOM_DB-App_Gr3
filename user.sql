83ebd5801e3c6dcb7e5bfd9a5fc5f47576c079a2
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-10-18 19:25:11 +0800

    Formatting of INSERT clauses

--- user.sql
+++ user.sql
@@ -14,16 +14,30 @@ CREATE TABLE user (
     loyalty_points 		BIGINT DEFAULT 0
 );
 
--- USERS who have just 
+-- USERS who have just started
+INSERT INTO user (first_name, last_name, email)
+VALUES
+	('Ramon', 'Biyaya', 'ramonbiyaya@gmail.com)',
+	('Rex', 'Dimalanta', 'rexdimalanta@gmail.com');
 
--- VALUES with all columns
+
+-- USERS who have been golfing already w/ full details
 INSERT INTO user (first_name, middle_name, last_name, email, contact, membership_tier, membership_start, membership_end, months_subscribed, loyalty_points)
 VALUES
-	();
-    
--- VALUES without middle_name column
+	('Sam', 'Concepcion', 'Liwanag', 'samliwanag@gmail.com', '09129299349', 'Silver', '2025-01-01', '2026-01-01', 9, 180);
+
+
+-- USERS who have been golfing already without middle_name
 INSERT INTO user (first_name, last_name, email, contact, membership_tier, membership_start, membership_end, months_subscribed, loyalty_points)
 VALUES
-	();
+	('John', 'Lenoba', 'john_lenoba@gmail.com', '09193229394', 'Diamond', '2020-01-01', '2026-01-01', 72, 5489);
     
--- 
+    
+-- USERS who decided to subscribe
+UPDATE user
+SET 
+	membership_tier = 'Silver',
+    membership_start = '2023-11-01',
+    membership_end = '2024-11-01',
+    months_subscribed = 12
+WHERE email = 'ramonbiyaya@gmail.com';
