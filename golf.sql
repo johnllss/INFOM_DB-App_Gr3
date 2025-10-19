75c5c3a09f11cc1c3e381bfde7fa003ef914ec29
Jerry <jerrykingdeveza@gmail.com>
2025-10-19 19:05:11 +0800

    cart_item

--- golf.sql
+++ golf.sql
@@ -35,3 +35,37 @@ CREATE TABLE session (
     FOREIGN KEY			(payment_id)	REFERENCES payment(payment_id),
     FOREIGN KEY			(user_id)		REFERENCES user(user_id)
 );
+
+CREATE TABLE cart(
+    cart_id         BIGINT PRIMARY KEY,
+    items_price     DECIMAL(10, 2) DEFAULT 0 NOT NULL
+);
+
+CREATE TABLE item(
+    item_id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name            VARCHAR(100) NOT NULL,
+    type            ENUM() NOT NULL,
+    quantity        INT DEFAULT(1) NOT NULL,
+    price           DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY     (cart_id)   REFERENCES cart(cart_id)
+
+);
+
+
+DELIMITER //
+
+CREATE TRIGGER update_items_price
+AFTER INSERT OR UPDATE OR DELETE FROM ON item
+FOR EACH ROW 
+BEGIN
+    UPDATE cart
+    SET items_price = (
+        SELECT  SUM(price * quantity)
+        FROM    item
+        WHERE   cart_id = NEW.cart_id
+    )
+    WHERE   cart_id = NEW.cart_id;
+END;
+//
+
+DELIMITER;
