fb5b13cdeb15e3b5c81b04b7ae9d49b28ee09bc9
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2025-10-19 19:36:33 +0800

    Migrated user table to golf.sql + added ENUM 'Unsubscribed'

--- golf.sql
+++ golf.sql
@@ -2,6 +2,20 @@ CREATE SCHEMA IF NOT EXISTS dbgolf;
 
 USE dbgolf;
 
+CREATE TABLE user (
+	user_id 			BIGINT PRIMARY KEY AUTO_INCREMENT,
+    first_name  		VARCHAR(100) NOT NULL,
+    middle_name 		VARCHAR(100) NULL,
+    last_name 			VARCHAR(100) NOT NULL,
+    email				VARCHAR(255) NOT NULL UNIQUE,
+    contact				VARCHAR(20) NULL,
+    membership_tier 	ENUM('Unsubscribed', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond') DEFAULT 'Unsubscribed',
+    membership_start 	DATE,
+    membership_end 		DATE,
+    months_subscribed 	BIGINT,
+    loyalty_points 		BIGINT DEFAULT 0
+);
+
 CREATE TABLE payment (
 	payment_id			BIGINT PRIMARY KEY AUTO_INCREMENT,
   	total_price			DECIMAL(8,2) NOT NULL,
