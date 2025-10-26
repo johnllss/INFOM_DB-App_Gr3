57966821ff2b5eeb708afebf134c160d615d0f9f
gab <101641170+gabespineli@users.noreply.github.com>
2025-10-26 21:20:27 +0800

    Modify golf.sql to enhance session and user tables

--- golf.sql
+++ golf.sql
@@ -30,7 +30,8 @@ CREATE TABLE staff (
     name        VARCHAR(100) NOT NULL,
     email       VARCHAR(100) NOT NULL,
     contact     VARCHAR(20) NOT NULL,
-    role        VARCHAR(50) NOT NULL,
+    max_clients INT DEFAULT 1,
+    role        ENUM('Caddie','Coach') NOT NULL,
     status      ENUM('Available', 'Occupied') NOT NULL DEFAULT 'Available',
     service_fee DECIMAL(8,2) NOT NULL
 );
@@ -39,17 +40,26 @@ CREATE TABLE session (
 	    session_id          INT PRIMARY KEY AUTO_INCREMENT,
 	    type                ENUM('Driving Range','Fairway'),
 	    holes               ENUM('Half 9','Full 18'),
-	    buckets             INT,
 	    session_schedule    DATETIME NOT NULL,
-	    people_quantity     INT NOT NULL,
-	    status              ENUM('Cancelled','Confirmed','Ongoing','Finished') NOT NULL,
-	    session_price       DECIMAL(10,2) DEFAULT 0,
-		user_id             INT NOT NULL,
-	    staff_id            INT,
-    CONSTRAINT fk_session_user FOREIGN KEY (user_id) REFERENCES user(user_id),
-    CONSTRAINT fk_session_staff FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
+	    people_limit	    INT,
+	    status              ENUM('Available','Fully Booked','Ongoing','Finished') NOT NULL,
+	    session_price       DECIMAL(10,2) DEFAULT 0
 );
 
+CREATE TABLE session_user (
+		session_user_id		INT PRIMARY KEY AUTO_INCREMENT,
+        user_id             INT,
+	    session_id          INT NOT NULL,
+        staff_id			INT,
+	    score_fairway       INT DEFAULT NULL,
+	    longest_range       INT DEFAULT NULL,
+        buckets             INT,
+        loyalty_earned		INT DEFAULT 0,
+	UNIQUE (session_id, user_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(user_id),
+    CONSTRAINT fk_session FOREIGN KEY (session_id) REFERENCES session(session_id),
+    CONSTRAINT fk_staff FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
+);
 
 CREATE TABLE item (
 	    item_id     INT PRIMARY KEY AUTO_INCREMENT,
@@ -57,7 +67,7 @@ CREATE TABLE item (
 	    type        ENUM('Sale','Rental') NOT NULL,
 	    quantity    INT DEFAULT 1 NOT NULL,
 	    price       DECIMAL(10,2) NOT NULL,
-		cart_id     INT,
+		cart_id     INT NULL,
     CONSTRAINT fk_item_cart FOREIGN KEY (cart_id) REFERENCES cart(cart_id)
 );
 
@@ -71,10 +81,10 @@ CREATE TABLE payment (
 	    discount_applied    DECIMAL(5,2) DEFAULT 0.0,
 		user_id             INT NOT NULL,
 	    cart_id             INT,
-	    session_id          INT,
+	    session_user_id     INT,
     CONSTRAINT fk_payment_user FOREIGN KEY (user_id) REFERENCES user(user_id),
     CONSTRAINT fk_payment_cart FOREIGN KEY (cart_id) REFERENCES cart(cart_id),
-    CONSTRAINT fk_payment_session FOREIGN KEY (session_id) REFERENCES session(session_id)
+    CONSTRAINT fk_payment_session_user FOREIGN KEY (session_user_id) REFERENCES session_user(session_user_id)
 );
 
 SET FOREIGN_KEY_CHECKS = 1;
